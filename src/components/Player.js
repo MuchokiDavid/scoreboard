1c9d7d2f4ae19641c3278ebf3d706f39e9cdf349
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-11 18:00:02 +0300

    Improved list

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,19 +5,44 @@ import Header from './Header';
 import { FaCrown } from "react-icons/fa";//black
 import { LuCrown } from "react-icons/lu";//white
 import { playerList } from '../data/ListPlayers';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Button } from 'react-bootstrap';
 
 
 function Player() {
   const [player, setPlayer] = useState(playerList);
+  const [count , setCount]= useState(0)
 
   const handleSubmit = (data) => {
     setPlayer ((prevList) => [...prevList, data])
   }
 
+  //function handle increase
+  const handleAdd= () => {
+    setCount(count + 1)
+  }
+
+  // function handle Subtract
+  const handleSubtract = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
   const playersList= player.map((p, index)=> {
     return (
-      <div key={index} className="bg-white">
-        <p>{p}</p>
+      <div key={index} className="bg-white divide-y divide-dashed">
+        <Container>
+          <Row>
+            <Col><LuCrown /></Col>
+            <Col><p>{p}</p></Col>
+            <Col><Button onClick={handleAdd}>+</Button>{count}<Button onClick={handleSubtract}>-</Button></Col>
+            <Col><Button>Remove</Button></Col>
+          </Row>
+        </Container>
+        
       </div>
     )
   })
@@ -35,3 +60,73 @@ function Player() {
 }
 
 export default Player
+
+// import React, { useState } from 'react';
+// import Footer from './Footer';
+// import Header from './Header';
+// import { LuCrown } from 'react-icons/lu'; // white
+// import { playerList } from '../data/ListPlayers';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import { Button } from 'react-bootstrap';
+
+// function Player() {
+//   const [players, setPlayers] = useState(playerList.map(() => ({ count: 0 })));
+
+//   const handleSubmit = (data) => {
+//     setPlayers((prevPlayers) => [...prevPlayers, data{ count: 0 }]);
+//   };
+
+//   const handleAdd = (index) => {
+//     setPlayers((prevPlayers) =>
+//       prevPlayers.map((player, i) =>
+//         i === index ? { ...player, count: player.count + 1 } : player
+//       )
+//     );
+//   };
+
+//   const handleSubtract = (index) => {
+//     setPlayers((prevPlayers) =>
+//       prevPlayers.map((player, i) =>
+//         i === index && player.count > 0
+//           ? { ...player, count: player.count - 1 }
+//           : player
+//       )
+//     );
+//   };
+
+//   const playersList = players.map((player, index) => (
+//     <div key={index} className="bg-white divide-y divide-dashed">
+//       <Container>
+//         <Row>
+//           <Col>
+//             <LuCrown />
+//           </Col>
+//           <Col>
+//             <p>{playerList[index]}</p>
+//           </Col>
+//           <Col>
+//             <Button onClick={() => handleAdd(index)}>+</Button>
+//             {player.count}
+//             <Button onClick={() => handleSubtract(index)}>-</Button>
+//           </Col>
+//           <Col></Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   ));
+
+//   return (
+//     <div>
+//       <Header />
+//       <div>
+//         <h1>Player</h1>
+//         {playersList}
+//       </div>
+//       <Footer handleForm={handleSubmit} />
+//     </div>
+//   );
+// }
+
+// export default Player;
--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -17,6 +17,7 @@ function PlayerForm({handleForm}) {
     function handleSubmit(e){
         e.preventDefault()
         handleForm(name)
+        setName("")
     }
     
   return (
@@ -28,7 +29,7 @@ function PlayerForm({handleForm}) {
                 name='name'
                 placeholder= "ENTER A PLAYER'S NAME"
                 value={name}
-                onChange={(e)=> setName(e.target.value)}
+                onChange={(e)=> setName(e.target.value.toLocaleUpperCase())}
                 className='block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 /></label>
                 <Button variant="primary" type="submit" onClick={handleSubmit}>ADD PLAYER</Button>
